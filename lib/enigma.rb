78de2c7b5b7e01c6a6872c74bdae31ae4243ee50
Jacob Volpe <volpe.jacob@gmail.com>
2021-01-15 14:11:47 -0700

    Move code from module into class, generate random 5 digit #, pass tests

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,13 +1,26 @@
+require 'date'
+require_relative './encryption_module'
+
 class Enigma 
-attr_reader :starter_message
+  # include Encryptable
+  attr_reader :starter_message
 
 
-def initialize(arg1)
-   @starter_message = get_starter_message(arg1)
-end
+  def initialize(arg1)
+     @starter_message = get_starter_message(arg1)
+  end
 
   def get_starter_message(arg)
     new_file = File.read(arg)
+  end
+
+  def generate_five_digit_number
+    number = rand.to_s[2..6]
+  end
+
+  def make_starter_array
+    code = generate_five_digit_number
+    array = [@starter_message, code]
   end
 
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -3,6 +3,7 @@ require 'date'
 require 'pry'
 require './lib/enigma'
 
+
 class EnigmaTest < Minitest::Test 
   def test_it_exists
     enigma = Enigma.new("./message.txt")
@@ -15,6 +16,15 @@ class EnigmaTest < Minitest::Test
     enigma = Enigma.new("./message.txt")
 
     assert_equal  "hello world", enigma.starter_message
+  end
+
+  def test_make_starter_array
+    enigma = Enigma.new("./message.txt")
+
+    assert_equal Array, enigma.make_starter_array.class
+    assert_equal String, enigma.make_starter_array[0].class
+    assert_equal String, enigma.make_starter_array[1].class
+    assert_equal 5, enigma.make_starter_array[1].length
   end
 
 end
