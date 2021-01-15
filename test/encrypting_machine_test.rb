3e64f28b21162bbf5ae2edadc75771f0b78dced4
Jacob Volpe <volpe.jacob@gmail.com>
2021-01-15 16:48:56 -0700

    Write more initial tests for machine

--- test/encrypting_machine_test.rb
+++ test/encrypting_machine_test.rb
@@ -3,11 +3,18 @@ require 'date'
 require 'pry'
 require './lib/enigma'
 require './lib/encrypting_machine'
+require 'mocha/minitest'
+
 
 class EncryptingMachineTest < Minitest::Test 
   def test_it_exists_and_has_attributes
-    machine = EncryptingMachine.new(["hello world", "09547", "150121"])
+    enigma = mock
+    machine = EncryptingMachine.new(["hello world", "09547", "150121"], enigma)
 
     assert_instance_of EncryptingMachine, machine
+    assert_equal "hello world", machine.plain_message
+    assert_equal "09547", machine.code
+    assert_equal "150121", machine.date
+    assert_equal enigma, machine.enigma
   end
 end
