f9814adf7d586cef02019abb5fdac31355de7868
Jacob Volpe <volpe.jacob@gmail.com>
2021-01-14 18:01:11 -0700

    Create blank enigma class, begin plan for strucutre

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,12 +1,13 @@
 require_relative './test_helper'
 require 'date' 
 require 'pry'
+require './lib/enigma'
 
 class EnigmaTest < Minitest::Test 
   def test_it_exists
     enigma = Enigma.new
 
-    asssert_instance_of Enigma, enigma
+    assert_instance_of Enigma, enigma
   end
 
 end
