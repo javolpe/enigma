956a7e980686711c90e5a42542a10575fa1601dc
Jacob Volpe <volpe.jacob@gmail.com>
2021-01-16 16:26:01 -0700

    Refactor, encrypt method name, takes optional args, change tests to update

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,7 +2,6 @@ require 'date'
 require_relative './encrypting_machine'
 
 class Enigma 
-  # include Encryptable
   attr_reader :starter_message
 
 
@@ -15,28 +14,25 @@ class Enigma
     unencoded_message.downcase
   end
 
-  def generate_five_digit_number
+  def generate_code
     number = rand.to_s[2..6]
   end
 
-  def generate_date_stamp
+  def generate_date
     date = Time.now.strftime("%d%m%Y")
     date = date[0..3]+date[6..8]
   end
 
-  def make_starter_array
-    code = generate_five_digit_number
-    date = generate_date_stamp
-    array = [@starter_message, code, date]
+  def message 
+    @starter_message
   end
 
-  def put_into_encrypting_machine
-     start = make_starter_array
-     machine = EncryptingMachine.new(start, self)
-     encrypted = machine.decide_encryption_path
-    final_array = [encrypted]
-    final_array << machine.code
-    final_array << machine.date
+  def encrypt(message, code = generate_code, date = generate_date)
+    machine = EncryptingMachine.new(message, code, date)
+    encrypted = machine.decide_encryption_path
+    final_hash = {encryption: encrypted,
+                  key: code,
+                  date: date}
   end
 
 end
--- test/encrypting_machine_test.rb
+++ test/encrypting_machine_test.rb
@@ -8,19 +8,16 @@ require 'mocha/minitest'
 
 class EncryptingMachineTest < Minitest::Test 
   def test_it_exists_and_has_attributes
-    enigma = mock
-    machine = EncryptingMachine.new(["hello world!", "09547", "150121"], enigma)
+    machine = EncryptingMachine.new("hello world!", "09547", "150121")
 
     assert_instance_of EncryptingMachine, machine
     assert_equal "hello world!", machine.plain_message
     assert_equal "09547", machine.code
     assert_equal "150121", machine.date
-    assert_equal enigma, machine.enigma
   end
 
   def test_base
-    enigma = mock
-    machine = EncryptingMachine.new(["hello world!", "09547", "150121"], enigma)
+    machine = EncryptingMachine.new("hello world!", "09547", "150121")
 
     assert_equal Array, machine.base.class 
     assert_equal "a", machine.base[0]
@@ -28,8 +25,7 @@ class EncryptingMachineTest < Minitest::Test
   end
 
   def test_shifter
-    enigma = mock
-    machine = EncryptingMachine.new(["hello world!", "09547", "150121"], enigma)
+    machine = EncryptingMachine.new("hello world!", "09547", "150121")
 
     assert_equal "4641", machine.shifter
     assert_equal String, machine.shifter.class
@@ -37,8 +33,7 @@ class EncryptingMachineTest < Minitest::Test
   end
 
   def test_shift_hash
-    enigma = mock
-    machine = EncryptingMachine.new(["hello world!", "09547", "150121"], enigma)
+    machine = EncryptingMachine.new("hello world!", "09547", "150121")
 
     expected = {:A => 13,
                 :B => 101,
@@ -51,8 +46,7 @@ class EncryptingMachineTest < Minitest::Test
   end
 
   def test_encoder_hash
-    enigma = mock
-    machine = EncryptingMachine.new(["hello world!", "09547", "150121"], enigma)
+    machine = EncryptingMachine.new("hello world!", "09547", "150121")
 
     assert_equal Hash, machine.encoder_hash(:A).class
     assert_equal String, machine.encoder_hash(:B)["a"].class
@@ -64,34 +58,25 @@ class EncryptingMachineTest < Minitest::Test
   end
 
   def test_message_divisible_by_four?
-    enigma = mock
-    machine = EncryptingMachine.new(["hello world!", "09547", "150121"], enigma)
+    machine = EncryptingMachine.new("hello world!", "09547", "150121")
     
     assert_equal true, machine.message_divisible_by_four?
 
-    enigma = mock
-    machine = EncryptingMachine.new(["hello world!&@", "09547", "150121"], enigma)
+    machine = EncryptingMachine.new("hello world!&@", "09547", "150121")
     
     assert_equal false, machine.message_divisible_by_four?
   end
 
-  # def test_decide_encryption_path
-  # not sure how to test this one but shit works
-  #if an instructor sees this I swear I did TDD for the rest like a good Turing student
-  # end
-
   def test_encrypt_divisible_by_four
-    enigma = mock
-    machine = EncryptingMachine.new(["hello world!", "09547", "150121"], enigma)
+    machine = EncryptingMachine.new("hello world!", "09547", "150121")
 
-    assert_equal "uypfat ideh!", machine.encyrpt_divisible_by_four
-    assert_equal String, machine.encyrpt_divisible_by_four.class
-    assert_equal machine.plain_message.length, machine.encyrpt_divisible_by_four.length
+    assert_equal "uypfat ideh!", machine.encrypt_divisible_by_four
+    assert_equal String, machine.encrypt_divisible_by_four.class
+    assert_equal machine.plain_message.length, machine.encrypt_divisible_by_four.length
   end
 
   def test_encode_letter
-    enigma = mock
-    machine = EncryptingMachine.new(["hello world!", "09547", "150121"], enigma)
+    machine = EncryptingMachine.new("hello world!", "09547", "150121")
 
     assert_equal "n", machine.encode_letter(:A, "a")
     assert_equal "u", machine.encode_letter(:B, "a")
@@ -100,15 +85,13 @@ class EncryptingMachineTest < Minitest::Test
   end
 
   def test_encrypt_NOT_divisible_by_four
-    enigma = mock
-    machine = EncryptingMachine.new(["hello world!au@", "09547", "150121"], enigma)
+    machine = EncryptingMachine.new("hello world!au@", "09547", "150121")
 
     assert_equal "uypfat ideh!nn@", machine.encrypt_NOT_divisible_by_four
   end
 
   def test_encrypt_dynamic_number_of_letters
-    enigma = mock
-    machine = EncryptingMachine.new(["hello world!au@", "09547", "150121"], enigma)
+    machine = EncryptingMachine.new("hello world!au@", "09547", "150121")
 
     assert_equal "nn@", machine.encrypt_dynamic_number_of_letters("au@")
     assert_equal String, machine.encrypt_dynamic_number_of_letters("au@").class
