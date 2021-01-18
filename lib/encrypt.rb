602c314f474f38353ee531ae42bd40da829499d1
Jacob Volpe <volpe.jacob@gmail.com>
2021-01-17 17:15:03 -0700

    Refactor encrypt to follow spec, read directions better next time

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,3 +1,11 @@
 require_relative './enigma'
 
-enigma = Enigma.new(ARGV[0], ARGV[1])
+enigma = Enigma.new
+message  = enigma.message(ARGV[0])
+code = enigma.generate_code(ARGV[0])
+date = enigma.generate_date(ARGV[0])
+enigma.message_file = ARGV[0]
+
+enigma.output_file = ARGV[1]
+
+enigma.encrypt(message)
